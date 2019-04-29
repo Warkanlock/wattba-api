f534f953f8b3f6cfb5b340951b036a916e849d9c
Michal Švácha <michal.svacha@gmail.com>
2019-04-29 11:17:18 -0700

    recommended dummy endpoint

--- quick/urls.py
+++ quick/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
 	# users
 	path('hello', views.hello_world),
 	path('hello/<name>/', views.hello_name),
+    #path('lessons/trending'),
+    path('lessons/recommended', views.recommended),
+    #path('lessons/<id>/comments'),
+    #path('lessons/<id>/detail'),
+    #path('lessons/<id>/files')
 ]
