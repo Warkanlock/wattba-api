42fc238a617fdb666899b531541fdb2209d94a29
sewagodimo <edneciamat@gmail.com>
2019-04-29 00:12:51 -0700

    add subject urls

--- api/urls.py
+++ api/urls.py
@@ -4,13 +4,16 @@ from . import views
 
 urlpatterns = [
 	# users
-	path('', views.LessonsList.as_view()),
+	path('', views.HomeView.as_view()),
 	path('user/<slug:pk>/', views.UserDetail.as_view()),
 	path('users/', views.UserList.as_view()),
 	path('new_users/', views.UserCreate.as_view()),
 
 	# subjects
-	
-	# lessons
+	path('subjects/', views.SubjectsList.as_view()),
+	path('subjects/<slug:pk>/', views.SubjectDetail.as_view()),
 
+	# lessons
+	path('lessons/', views.LessonsList.as_view()),
+	path('lessons/<slug:pk>/', views.LessonDetail.as_view()),
 ]
