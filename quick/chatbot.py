875066fd1a45ba77a267c019ba412f7e3829adaf
Ross Guy <RossG2@discovery.co.za>
2019-04-29 17:37:37 -0700

    added lesson id

--- quick/chatbot.py
+++ quick/chatbot.py
@@ -7,10 +7,12 @@ def detail(request, id):
     lesson = Lesson.objects.get(pk=id)
 
     data = {
+        "lesson_id": lesson.id,
         "title": lesson.title,
         "content": strip_tags(lesson.content),
         "summary": lesson.summary,
         "grade": lesson.grade,
+        "subject": lesson.subject.name,
         "image": "https://via.placeholder.com/1000x1000.png?text={}".format(lesson.title)
     }
 
@@ -29,6 +31,7 @@ def lessons_by_subject(request):
 
     for l in lessons:
         lesson = {
+            "lesson_id": l.id,
             "title": l.title,
             "content": strip_tags(l.content),
             "summary": l.summary,
